Barmada-Lab/cytomancer

commit 958e40390a94bc1a73e2604f5a3e6f53a3100341

change 'field' values to integers
'field' values for legacy experiments were tuples (0_0, 0_1, ...), which
are inconsistent with other high-throughput experiment loaders (CQ1.)

The benefit of using tuples for field values is the imaging order is
baked-in, so a stitching algorithm can operate based on the coordinate
data without having to calculate the tile positions given an imaging
order. This is a nice feature, but right now we're not doing much
stitching.

Eventually, it would be nice to have a standardized format for xarray
experiments, along with smarter zarr caching. For now, we'll just revert
field values to integers to be consistent.

cytomancer/io/legacy_loader.py
@@ -1,9 +1,7 @@
 import pathlib as pl
-from itertools import product
 
 import dask.array as da
 import xarray as xr
-import numpy as np
 
 from . import ioutils
 
@@ -11,21 +9,17 @@ from . import ioutils
 def load_legacy(base: pl.Path, fillna: bool) -> xr.DataArray:
     timepoint_tags = sorted({int(path.name.replace("T", "")) for path in base.glob("raw_imgs/*/*")})
     region_tags = set()
-    field_id_tags = set()
+    field_tags = set()
     channel_tags = set()
     for path in base.glob("raw_imgs/**/*.tif"):
         channel_tags.add(path.parent.parent.parent.name)
         region, field = path.name.split(".")[0].split("_")
         region_tags.add(region)
-        field_id_tags.add(field)
-
-    max_field_id = max(map(int, field_id_tags))
-    dim = np.sqrt(max_field_id).astype(int)
-    field_tags = list(map(lambda x: "_".join(map(str, x)), product(range(dim), range(dim))))
+        field_tags.add(str(int(field)))  # get rid of zero-padding
 
     region_tags = sorted(region_tags)
     channel_tags = sorted(channel_tags)
-    field_id_tags = sorted(field_id_tags)
+    field_tags = sorted(field_tags, key=lambda x: int(x))
     timepoint_tags = sorted(timepoint_tags)
 
     # TODO: SURE SEEMS LIKE THIS COULD BE MADE RECURSIVE DONT IT
@@ -37,7 +31,7 @@ def load_legacy(base: pl.Path, fillna: bool) -> xr.DataArray:
             regions = []
             for region in region_tags:
                 fields = []
-                for field in field_id_tags:
+                for field in field_tags:
                     col = region[1:]
                     path = base / "raw_imgs" / channel / f"T{timepoint}" / f"col_{col}" / f"{region}_{field}.tif"
                     img = ioutils.read_tiff_toarray(path)
@@ -67,21 +61,17 @@ def load_legacy(base: pl.Path, fillna: bool) -> xr.DataArray:
 def load_legacy_icc(base: pl.Path, fillna: bool) -> xr.DataArray:
     timepoint_tags = sorted({int(path.name.replace("T", "")) for path in base.glob("raw_imgs/*/*")})
     region_tags = set()
-    field_id_tags = set()
+    field_tags = set()
     channel_tags = set()
     for path in base.glob("raw_imgs/**/*.tif"):
         channel_tags.add(path.parent.parent.parent.name)
         region, field = path.name.split(".")[0].split("_")
         region_tags.add(region)
-        field_id_tags.add(field)
-
-    max_field_id = max(map(int, field_id_tags))
-    dim = np.sqrt(max_field_id).astype(int)
-    field_tags = list(map(lambda x: "_".join(map(str, x)), product(range(dim), range(dim))))
+        field_tags.add(str(int(field)))  # get rid of zero-padding
 
     region_tags = sorted(region_tags)
     channel_tags = sorted(channel_tags)
-    field_id_tags = sorted(field_id_tags)
+    field_tags = sorted(field_tags, key=lambda x: int(x))
     timepoint_tags = sorted(timepoint_tags)
 
     # TODO: SURE SEEMS LIKE THIS COULD BE MADE RECURSIVE DONT IT
@@ -93,7 +83,7 @@ def load_legacy_icc(base: pl.Path, fillna: bool) -> xr.DataArray:
             regions = []
             for region in region_tags:
                 fields = []
-                for field in field_id_tags:
+                for field in field_tags:
                     col = region[1:]
                     path = base / "raw_imgs" / channel / f"T{timepoint}" / f"col_{col}" / f"{region}_{field}.tif"
                     img = ioutils.read_tiff_toarray(path)

commit 7c89e1a9abd06d4a0691c5a714cca3fd5a5e8d70

fix issue introduced in 958e403

cytomancer/io/legacy_loader.py
@@ -15,7 +15,7 @@ def load_legacy(base: pl.Path, fillna: bool) -> xr.DataArray:
         channel_tags.add(path.parent.parent.parent.name)
         region, field = path.name.split(".")[0].split("_")
         region_tags.add(region)
-        field_tags.add(str(int(field)))  # get rid of zero-padding
+        field_tags.add(field)  # get rid of zero-padding
 
     region_tags = sorted(region_tags)
     channel_tags = sorted(channel_tags)
@@ -48,7 +48,7 @@ def load_legacy(base: pl.Path, fillna: bool) -> xr.DataArray:
             "channel": channel_tags,
             "time": timepoint_tags,
             "region": region_tags,
-            "field": field_tags,
+            "field": [str(int(field)) for field in field_tags],
         }
     )
 
@@ -67,7 +67,7 @@ def load_legacy_icc(base: pl.Path, fillna: bool) -> xr.DataArray:
         channel_tags.add(path.parent.parent.parent.name)
         region, field = path.name.split(".")[0].split("_")
         region_tags.add(region)
-        field_tags.add(str(int(field)))  # get rid of zero-padding
+        field_tags.add(field)  # get rid of zero-padding
 
     region_tags = sorted(region_tags)
     channel_tags = sorted(channel_tags)
@@ -100,7 +100,7 @@ def load_legacy_icc(base: pl.Path, fillna: bool) -> xr.DataArray:
             "channel": channel_tags,
             "time": [0],
             "region": list(map(str, timepoint_tags)),
-            "field": field_tags,
+            "field": [str(int(field)) for field in field_tags],
         }
     ).squeeze("time", drop=True)
 

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "cytomancer"
-version = "1.3.1"
+version = "1.3.2"
 description = ""
 authors = ["Jacob Waksmacki <jwaksmack@gmail.com>"]
 