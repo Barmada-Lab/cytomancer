6909c23fd8b7297778fbceab67bbfc0700fa2684
Jacob Waksmacki <jwaksmac@umich.edu>
2024-09-25 19:54:50 -0400

    fix regression

--- cytomancer/cvat/survival.py
+++ cytomancer/cvat/survival.py
@@ -52,7 +52,7 @@ def cli_entry(project_name: str, experiment_dir: pl.Path, well_csv: pl.Path | No
 
     if (upload_record_path := results_dir / "cvat_upload.csv").exists():
         task_df = pd.read_csv(upload_record_path).drop(columns=["time"])
-        task_df["frame"] = task_df["frame"].str.replace(r'_\d\.tif', '', regex=True)
+        task_df["frame"] = task_df["frame"].str.replace(r'_\d+\.tif', '', regex=True)
         task_df.rename(columns={"frame": "task_name"}, inplace=True)
         task_df.drop_duplicates(inplace=True)
     else:
@@ -65,6 +65,8 @@ def cli_entry(project_name: str, experiment_dir: pl.Path, well_csv: pl.Path | No
         .drop(columns=["task_name", "field"])
         .sort_values(["time", "dead", "well"])
     )
+
+    print(df)
 
     if well_csv is not None:
         well_df = pd.read_csv(well_csv)[["Vertex", "Condition"]]
@@ -83,3 +85,6 @@ def cli_entry(project_name: str, experiment_dir: pl.Path, well_csv: pl.Path | No
             drawstyle="steps-mid",)
         output_fig = results_dir / "CoxPH_baselines_CVAT.pdf"
         plt.savefig(output_fig, format="pdf")
+
+    else:
+        df.to_csv(survival_output, index=False)
