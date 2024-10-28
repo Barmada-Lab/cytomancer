62af1e73a03ceab59faf0755c0715dc887e3b025
Jacob Waksmacki <jwaksmac@umich.edu>
2024-10-28 16:38:55 -0400

    add alternate option to cvat survival

--- cytomancer/cvat/survival.py
+++ cytomancer/cvat/survival.py
@@ -42,7 +42,15 @@ def analyze_survival(tasks) -> pd.DataFrame:
     type=click.Path(exists=True, path_type=pl.Path),
     help="CSV file with well information",
 )
-def cli_entry(project_name: str, experiment_dir: pl.Path, well_csv: pl.Path | None):
+@click.option(
+    "--ignore-missing-upload", is_flag=True, help="Ignore missing upload record"
+)
+def cli_entry(
+    project_name: str,
+    experiment_dir: pl.Path,
+    well_csv: pl.Path | None,
+    ignore_missing_upload: bool,
+):
     client = new_client_from_config(config)
 
     if (project := get_project(client, project_name)) is None:
@@ -53,51 +61,52 @@ def cli_entry(project_name: str, experiment_dir: pl.Path, well_csv: pl.Path | No
 
     survival_output = results_dir / "cvat_survival.csv"
 
-    if (upload_record_path := results_dir / "cvat_upload.csv").exists():
+    upload_record_path = results_dir / "cvat_upload.csv"
+    if (upload_record_path).exists():
         task_df = pd.read_csv(upload_record_path).drop(columns=["time"])
         task_df["frame"] = task_df["frame"].str.replace(r"_\d+\.tif", "", regex=True)
         task_df.rename(columns={"frame": "task_name"}, inplace=True)
         task_df.drop_duplicates(inplace=True)
-    else:
-        raise ValueError(
-            f"Could not find {upload_record_path}; was this project uploaded with an older version of cytomancer? If so, use the legacy survival script."
+        df = (
+            analyze_survival(project.get_tasks())
+            .merge(task_df, on="task_name")
+            .rename(columns={"region": "well"})
+            .drop(columns=["task_name", "field"])
+            .sort_values(["time", "dead", "well"])
         )
+        if well_csv is not None:
+            well_df = pd.read_csv(well_csv)[["Vertex", "Condition"]]
+            well_df["well"] = well_df["Vertex"].str[-3:]
+            well_df = well_df.drop(columns=["Vertex"])
+            df = df.merge(well_df, on="well")
+            df.to_csv(survival_output, index=False)
+            condition_counts = df.groupby("Condition").size()
+            df["Condition"] += (
+                " (n=" + df["Condition"].map(condition_counts).astype(str) + ")"
+            )
+            cph = CoxPHFitter()
+            cph.fit(
+                df.drop(columns="well"),
+                duration_col="time",
+                event_col="dead",
+                strata="Condition",
+            )
+            cph.baseline_cumulative_hazard_.plot(
+                ylabel="Cumulative hazard",
+                xlabel="T",
+                title="Baseline cumulative hazards",
+                drawstyle="steps-mid",
+            )
+            output_fig = results_dir / "CoxPH_baselines_CVAT.pdf"
+            plt.savefig(output_fig, format="pdf")
 
-    df = (
-        analyze_survival(project.get_tasks())
-        .merge(task_df, on="task_name")
-        .rename(columns={"region": "well"})
-        .drop(columns=["task_name", "field"])
-        .sort_values(["time", "dead", "well"])
-    )
-
-    print(df)
-
-    if well_csv is not None:
-        well_df = pd.read_csv(well_csv)[["Vertex", "Condition"]]
-        well_df["well"] = well_df["Vertex"].str[-3:]
-        well_df = well_df.drop(columns=["Vertex"])
-        df = df.merge(well_df, on="well")
+    elif ignore_missing_upload:
+        df = analyze_survival(project.get_tasks()).sort_values(
+            ["task_name", "time", "dead"]
+        )
         df.to_csv(survival_output, index=False)
-        condition_counts = df.groupby("Condition").size()
-        df["Condition"] += (
-            " (n=" + df["Condition"].map(condition_counts).astype(str) + ")"
-        )
-        cph = CoxPHFitter()
-        cph.fit(
-            df.drop(columns="well"),
-            duration_col="time",
-            event_col="dead",
-            strata="Condition",
-        )
-        cph.baseline_cumulative_hazard_.plot(
-            ylabel="Cumulative hazard",
-            xlabel="T",
-            title="Baseline cumulative hazards",
-            drawstyle="steps-mid",
-        )
-        output_fig = results_dir / "CoxPH_baselines_CVAT.pdf"
-        plt.savefig(output_fig, format="pdf")
 
     else:
-        df.to_csv(survival_output, index=False)
+        raise FileNotFoundError(
+            f"Could not find {upload_record_path}; was this project uploaded with an older version of cytomancer? If so, use the legacy survival script"
+        )
