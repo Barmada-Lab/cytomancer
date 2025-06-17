1708e325e064d5d66992aac3a3e80eae4023d69d
Jacob Waksmacki <jwaksmac@umich.edu>
2025-06-17 12:28:13 +0900

    Create license; add acknowledgments

--- cytomancer/models/unet/train.py
+++ cytomancer/models/unet/train.py
@@ -1,3 +1,23 @@
+# This file is part of Cytomancer.
+#
+# Cytomancer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cytomancer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MyProject. If not, see <http://www.gnu.org/licenses/>.
+#
+# Portions of this code are based on the original project Pytorch-UNet,
+# developed by Milesi Alexandre. Pytorch-UNet is available at:
+# <https://github.com/milesial/Pytorch-UNet>
+#
+# Some modifications made by Jacob Waksmacki, 2025
 import logging
 from pathlib import Path
 
